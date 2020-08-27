13bd1e4aa38dd89d7ff7bd870a2112b8a8dc8301
Layon Martins <layonmartins03@gmail.com>
2020-08-27 10:00:49 -0300

    upadte the GazeTeleportInteraction scene

---
+++ README.md
@@ -0,0 +1,5 @@
+# Setup and test the new Unity 3D 2020.12f plugin XR Settings for Google Cardboard
+
+![](Unity3D2020.png)
+
+Reference: https://www.youtube.com/watch?v=OEP7sMwfZnE&list=PLiuDBLn1FK-QOmpA3Vs2JY0DVAj0GfFPn&index=1
--- Temp/StagingArea/Il2Cpp/il2cppOutput/lumpedcpp/Lump_libil2cpp_mono-runtime.cpp
+++
@@ -1,3 +0,0 @@
-#include "il2cpp-config.h"
-#include "/home/venturus/Unity3d/2020.1.2f1/Editor/Data/il2cpp/libil2cpp/mono-runtime/il2cpp-callbacks.cpp"
-#include "/home/venturus/Unity3d/2020.1.2f1/Editor/Data/il2cpp/libil2cpp/mono-runtime/il2cpp-mono-support.cpp"
